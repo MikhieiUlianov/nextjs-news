42ef98c4f9040055aaae473e0ba1bc7ad2ec5153
Mkh <example@gmail.com>
2025-07-27 21:00:18 +0200

    fetch-directly-in-cmp

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,39 +1,13 @@
-"use client";
-
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-type NewsType = {
-  id: string;
-  slug: string;
-  title: string;
-  image: string;
-  date: string;
-  content: string;
-}[];
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
 
-export default function NewsPage() {
-  const [news, setNews] = useState<NewsType>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  if (!response.ok) {
+    throw new Error("fetching news error");
+  }
 
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-
-      if (!response.ok) {
-        setError("fetching news error");
-        setIsLoading(false);
-      }
-      const res = await response.json();
-
-      setIsLoading(false);
-      setNews(res);
-    }
-  }, []);
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
+  const news = await response.json();
 
   return (
     <>
